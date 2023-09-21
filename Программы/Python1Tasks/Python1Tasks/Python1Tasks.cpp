4e5639f8872fc05e12a36e7fbd9a4ff782c45d5a
AlexRepkin <realpal30@gmail.com>
2023-09-21 08:17:36 +0300

    Finished program

--- Программы/Python1Tasks/Python1Tasks/Python1Tasks.cpp
+++ Программы/Python1Tasks/Python1Tasks/Python1Tasks.cpp
@@ -3,25 +3,8 @@
 #include <map>
 using namespace std;
 
-int lives = 3, max_lives = 3, bandages = 1;
-double luck = 0;
+int lives = 3, max_lives = 3, bandages = 1, luck_meter = 0, rooms = 0, walked = 0;
 map<string, int> items;
-
-void minigames() {
-    if (rand() % 2) {
-        int number;
-        cout << "You've encountered a minigame! Pick a number from 0 to 9.";
-        cin >> number;
-        for (int i = 0; i < 3; i++) {
-            if (rand() % 10 == number) cout << "Congratulations, you've won! Here is your prize: ";
-            prizes();
-            break;
-        }
-    }
-    else {
-
-    }
-}
 
 void prizes() {
     cout << "You've obtained...";
@@ -44,6 +27,7 @@ void prizes() {
     case 6: {
         int luck = rand() % 10;
         cout << "Amulet! It gave you some luck! ( +)"<<luck<<"% luck)";
+        luck_meter += luck;
         items.insert(pair<string, int>("Amulet", luck));
         break;
     }
@@ -51,11 +35,28 @@ void prizes() {
     case 8: {
         int luck = (rand() % 10) + 10;
         cout << "Four-leaf clover! It gave you some luck! ( +)" << luck << "% luck";
+        luck_meter += luck;
         items.insert(pair<string, int>("Four-leaf clover", luck));
         break;
     }
     case 9:
         cout << "Oh no! Rat stole your prize!";
+    }
+}
+
+void minigames() {
+    if (rand() % 2) {
+        int number;
+        cout << "You've encountered a minigame! Pick a number from 0 to 9.";
+        cin >> number;
+        for (int i = 0; i < 3; i++) {
+            if (rand() % 10 == number) cout << "Congratulations, you've won! Here is your prize: ";
+            prizes();
+            break;
+        }
+    }
+    else {
+
     }
 }
 
@@ -66,11 +67,64 @@ void inventory() {
     }
 }
 
-int room() {
-    return 
+void stats() {
+    cout << "Right now your stats are:\nLuck - " << luck_meter << "%\nMaximum lives - "<<max_lives<<"\nCurrent lives - "<<lives;
 }
 
-int main(){
-    cout << "Hello! Welcome to my little game, specially created for this task!\nMain idea in this game is to stay alive till the end.";
-    
+void room() {
+    walked += 1;
+    switch (rand() % 10) {
+    case 0:
+    case 1:
+    case 2: {
+        if (rand() % 100 < luck_meter) {
+            cout << "Enemy! You've lost one life! ( -1 life)";
+            lives--;
+            break;
+        }
+    }
+    case 3:
+    case 4:
+    case 5:
+    case 6: {
+        cout << "Empty room";
+        break;
+    }
+    case 7:
+    case 8: {
+        if (bandages > 0) {
+            char agree;
+            cout << "You've found an old lady. She asked you for some Bandages. Will you give them to her?\nRight now you have " << bandages << " Bandages.\n" << "Y/N?";
+            cin >> agree;
+            if (agree == 'Y' || agree == 'y') {
+                bandages--;
+                cout << "You gave her 1 Bandage. You now have " << bandages << " Bandages.";
+                luck_meter += 20;
+            }
+            else cout << "You've excused yourself and continued your journey.";
+        }
+        else cout << "Empty room";
+        break;
+    }
+    case 9:
+        cout << "You've found something lying on the floor.";
+        prizes();
+    }
+}
+
+int main() {
+    string choosing;
+    cout << "Hello! Welcome to my little game, specially created for this task!\nMain idea in this game is to stay alive till the end.\nHow many rooms do you want to go through?\nRooms = ";
+    cin >> rooms;
+    cout << "Great! You've started your journey.";
+    while (true) {
+        if (lives < 0 || walked == rooms) break;
+        cout << "\nChoose the door.\n1/ 2/ 3/ stats/ inventory?\n";
+        cin >> choosing;
+        if (choosing == "1" || choosing == "2" || choosing == "3") room();
+        else if (choosing == "stats") stats();
+        else if (choosing == "inv" || choosing == "inventory") inventory();
+        else cout << "Wrong input!";
+    }
+    cout << "\nYou've finished your journey.";
 }
